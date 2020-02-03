301678fbaacf0ec91dfd84dd07597f788be251d9
Harsha <ashharsha1999@gmail.com>
2020-02-03 12:11:24 +0530

    Added support for Swish Activation

--- ANN.py
+++ ANN.py
@@ -18,16 +18,17 @@ def predict_outputs(weights_mat, data_inputs, data_outputs, activation="relu"):
                 r1 = relu(r1)
             elif activation == "sigmoid":
                 r1 = sigmoid(r1)
+            elif activation == "swish":
+                r1 = r1 * sigmoid(r1)
         predicted_label = numpy.where(r1 == numpy.max(r1))[0][0]
         predictions[sample_idx] = predicted_label
     correct_predictions = numpy.where(predictions == data_outputs)[0].size
     accuracy = (correct_predictions/data_outputs.size)*100
     return accuracy, predictions
-    
+
 def fitness(weights_mat, data_inputs, data_outputs, activation="relu"):
     accuracy = numpy.empty(shape=(weights_mat.shape[0]))
     for sol_idx in range(weights_mat.shape[0]):
         curr_sol_mat = weights_mat[sol_idx, :]
         accuracy[sol_idx], _ = predict_outputs(curr_sol_mat, data_inputs, data_outputs, activation=activation)
     return accuracy
-
